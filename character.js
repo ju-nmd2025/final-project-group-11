c719926b11e1f630c905540fb244aa5f4591451d
Haowen Long <loha24jv@student.ju.se>
2025-12-12 16:36:18 +0100

    Replace character as a emoji

--- character.js
+++ character.js
@@ -5,15 +5,45 @@ class Character {
     this.w = w;
     this.h = h;
     this.vy = 0;
-    this.gravity = 0.4; // Gravity effect
-    this.jumpForce = -10; // Jump strength
-    this.speed = 5; // Horizontal movement speed
+    this.gravity = 0.4;
+    this.jumpForce = -10;
+    this.speed = 5;
   }
 
   draw() {
     push();
-    fill(221, 160, 221);
-    ellipse(this.x + this.w / 2, this.y + this.h / 2, this.w, this.h);
+    translate(this.x + this.w / 2, this.y + this.h / 2);
+    scale(this.w / 250);
+    translate(-200, -200);
+
+    fill(255, 200, 50);
+    strokeWeight(0);
+    circle(200, 200, 250);
+
+    fill(255);
+    ellipse(150, 160, 80, 30);
+    ellipse(250, 160, 80, 30);
+
+    fill(0, 0, 0);
+    circle(130, 160, 15);
+    circle(225, 160, 15);
+
+    stroke(0);
+    strokeWeight(5);
+    noFill();
+    arc(140, 130, 60, 30, PI, TWO_PI);
+    arc(260, 130, 60, 30, PI, TWO_PI);
+
+    noFill();
+    stroke(0);
+    strokeWeight(5);
+    arc(200, 250, 155, 90, 0, PI);
+
+    fill(255, 150, 150, 150);
+    noStroke();
+    ellipse(120, 200, 50, 30);
+    ellipse(280, 200, 50, 30);
+
     pop();
   }
 
